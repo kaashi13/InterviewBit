db2f3bcca7e0ec75b48f71b2dd4d422a6a140945
kaashi13 <49022177+kaashi13@users.noreply.github.com>
2019-07-17 19:56:37 +0530

    Update AntiDiagonals.cpp

--- AntiDiagonals.cpp
+++ AntiDiagonals.cpp
@@ -3,9 +3,12 @@ vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
     int size = 2 * A.size() - 1;
     int n = A.size();
     
+    /* As the matrix is square, resultant array will have increasing size until it reaches N and then decreases symmitrically; hence 'size' */
     for(int i = 0 ; i < size ; i++) {
         vector<int> eachDiagonal;
         int colIndex, rowIndex, count = 0;
+        
+        /* Depending upon the current i value we will decide the size of the array and corresponding starting indexes from where start reading the matrix */
         if(i <= (n - 1)) {
             eachDiagonal.resize(i + 1);
             colIndex = i;
@@ -17,6 +20,7 @@ vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
             rowIndex = i - n + 1;
         }
         
+        /* Now once all the indexes are set, we just need to start reading elements diagonally until we reach end of that diagonal(determined by this condition) */
         while((colIndex >= 0 && colIndex < n) && (rowIndex >= 0 && rowIndex < n)) {
             eachDiagonal[count] = A[rowIndex][colIndex];
             rowIndex++;
